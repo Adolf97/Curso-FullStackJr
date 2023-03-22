0e12ac6c880f00f4fa81ad9b70e695b7862eed89
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-03-21 21:19:16 -0600

    main.js: se agregó la función para eliminar los elementos del listado

--- main.js
+++ main.js
@@ -12,5 +12,12 @@ window.onload = () => {
     const todoList = document.getElementById("todo-list");
     const todosTemplate = todos.map(t => "<li>" + t + "</li>");
     todoList.innerHTML = todosTemplate.join("");
+
+    const elements = document.querySelectorAll("#todo-list li");
+    elements.forEach((elements, i) => {
+      elements.addEventListener('click', () => {
+        elements.parentNode.removeChild(elements);
+      })
+    })
   }
 }
