fe87905d0ceecc0c722f4a84c4733bc33b4fe3f0
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-03-22 19:21:31 -0600

    agregué CATCH al archivo de promesas

--- Tips Javascript/promesas.js
+++ Tips Javascript/promesas.js
@@ -3,3 +3,7 @@ Promise.resolve(2)
 // Estas promesas pueden irse "encadenando", es decir, que puedes sumarle o restarle
 Promise.resolve(valor)
     .then(valor => console.log(valor)); // 3
+
+/////CATCH
+Promise.reject(2) // Con el .reject lo que hacemos es que se salte hasta el catch
+    .catch(e => console.error(e)); // Esto nos imprime un mensaje de error en la consola
