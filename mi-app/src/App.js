19537ffc1a2cd769b874488aa2144006b3851ffc
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-04-18 22:50:09 -0600

    Agregué funciones onClick en el botón y en el H1

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,29 +1,16 @@
-import './App.css';
-
-const estilos = (bg = '#333') => ({
-  backgroundColor: bg,
-  color: "#fff",
-  padding: "15px 25px",
-  margin: "20px 0",
-})
-
-const estilos2 = {
-  boxShadow: "0 5px 5px #000",
-}
-
-const Li = ({ children }) => {
-  return(
-    <li style={{ ...estilos(), ...estilos2 }} className='clase-li'>{children}</li>
-  )
-}
+import Button from "./Button"
 
 const App = () => {
-  const valor = 'triste'
-  return (
-    <ul style={estilos('#056')} className='clase-css'>
-      <Li estado='feliz'>valor de li: {valor}</Li>
-    </ul>
-  );
+    return(
+        <div>
+            <h1 onClick={(e) => console.log("click", e)}>
+                Hola Mundo
+            </h1>
+            <Button onClick={() => console.log("clickeado")}>
+                Enviar
+            </Button>
+        </div>
+    )
 }
 
-export default App;
+export default App
