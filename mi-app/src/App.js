a46f5e42cc19279a134fd9e99994f83b8770731f
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-04-18 23:02:22 -0600

    Agregué una condicional y un arreglo para hacer pruebas

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,11 +1,23 @@
 import Button from "./Button"
 
+const arr = [
+    'chanchito triste',
+    'chanchito feliz',
+    'chanchito emocionado',
+]
+
 const App = () => {
+    const miVariable = false
+
+    if(miVariable) {
+        return <p>Mi variable dio true</p>
+    }
     return(
         <div>
             <h1 onClick={(e) => console.log("click", e)}>
                 Hola Mundo
             </h1>
+            {arr.map(el => <p key={el}>{el}</p>)}
             <Button onClick={() => console.log("clickeado")}>
                 Enviar
             </Button>
